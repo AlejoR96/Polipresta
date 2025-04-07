c0878dfc70966639e42cf85688ff207c6b646d9a
Alejandro Rivera <jalejandro142@gmail.com>
2025-04-06 23:12:39 -0500

    Se finaliza el login falta mejorar color y agregar un boton adicional

--- app/src/main/java/com/example/polipresta/MainActivity.kt
+++ app/src/main/java/com/example/polipresta/MainActivity.kt
@@ -16,17 +16,7 @@ import com.example.polipresta.ui.theme.PoliprestaTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            PoliprestaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.login)
     }
 }
 
